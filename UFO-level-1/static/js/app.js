a4f571b8921c3ecb597f985ee1e541c3be8add32
Tim Weir <timothy.weir@comcast.net>
2021-05-02 11:30:15 -0400

    Part 1 Req 1

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,13 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
